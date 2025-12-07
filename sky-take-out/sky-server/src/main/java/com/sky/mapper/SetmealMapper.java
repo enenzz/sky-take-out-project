89f1a0e9d722ec1f30d9de9ca83a4282aa09b894
enenzz <2669819072@qq.com>
2025-12-07 13:42:18 +0800

    套餐分页查询业务代码开发

--- sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-take-out/sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,9 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
 import com.sky.annotation.AutoFill;
 import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
 import com.sky.entity.Setmeal;
 import com.sky.enumeration.OperationType;
+import com.sky.vo.SetmealVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
@@ -39,4 +42,11 @@ public interface SetmealMapper {
      */
     @AutoFill(value = OperationType.UPDATE)
     void update(Setmeal setmeal);
+
+    /**
+     * 分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    Page<SetmealVO> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO);
 }
